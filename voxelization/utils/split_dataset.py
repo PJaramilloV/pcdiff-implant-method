7055522d2de18eac8088ac111ac44f5f0dc00e66
PJaramilloV <pablojaramillovesperinas@gmail.com>
2023-11-11 18:46:25 -0300

    feat: vox's data split generalized

---
+++ voxelization/utils/split_dataset.py
@@ -0,0 +1,46 @@
+import os
+import csv
+import math
+import random
+import argparse
+
+data = []
+defects = ['broken']
+data_dir = 'data/pjaramil/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='_bottles', help="set dataset name")
+opt = parser.parse_args()
+dataset = opt.dataset
+voxelization = 'voxelization'
+
+with open(os.path.join(data_dir, dataset, 'train.csv'), 'r', newline='') as file:
+    csvreader = csv.reader(file)
+    objects = 0
+    for row in csvreader:
+        split = row[0].split('complete')
+        path = split[0]
+        file = split[1].split('.obj')[0][1:] # /file.obj
+        data.append(os.path.join(path, voxelization, file))
+        objects += 1
+    print(f"Found {objects} {dataset} scans")
+
+train = random.sample(data, math.ceil(len(data)*0.70))
+test = [elem for elem in data if elem not in train]
+
+os.makedirs(os.path.join(data_dir, dataset, voxelization), exist_ok=True)
+
+# Training set
+with open(os.path.join(data_dir, dataset, voxelization,'train.csv'), 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range(len(train)):
+        datapoint = train[i]
+        writer.writerow([datapoint])
+
+# Test set
+with open(os.path.join(data_dir, dataset, voxelization, 'eval.csv'), 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range(len(test)):
+        datapoint = test[i]
+        writer.writerow([datapoint])
+
+print(f"Successfully created training and evaluation split for {dataset}")
